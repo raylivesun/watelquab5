0c4342264a09ccdf10107782dcb0d9bf16bb1d24
raylivesun <162396999+raylivesun@users.noreply.github.com>
2025-05-14 16:25:39 -0300

    the main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <array>
+#include <string>
+#include <langinfo.h>
+#include <aliases.h>
+#include <assert.h>
+#include <iostream>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!";
+    return 0;
+}
